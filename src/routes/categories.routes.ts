0ae64c4b9860d49d4027e98e278736c2d2c1a627
jvkass <jvcnmata@gmail.com>
2022-05-22 11:05:38 -0300

    Criação do UseCase e aplicação do Singleton Pattern

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
-
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response)=>{
+categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response)
 });
 
-categoriesRoutes.get("/",(request,response)=>{
-    const all = categoriesRepository.list();
-
-    return response.json(all);
+categoriesRoutes.get("/", (request, response) => {
+    return listCategoriesController.handle(request, response);
 })
 
-export {categoriesRoutes};
+export { categoriesRoutes };
 
