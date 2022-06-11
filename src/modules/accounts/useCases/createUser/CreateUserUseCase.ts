01f331707148c0be39c15fc6b72d2052c769a48c
jvkass <jvcnmata@gmail.com>
2022-06-11 16:20:34 -0300

    Criação do repository do user

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { inject } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserDTO } from "../../dtos/ICreatedUserDTO";
+
+class CreateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+
+    async execute({
+        name,
+        username,
+        email,
+        password,
+        driver_license,
+    }:ICreateUserDTO): Promise<void>{
+        await this.usersRepository.create({
+            name,
+            username,
+            email,
+            password,
+            driver_license
+        });
+    }
+}
