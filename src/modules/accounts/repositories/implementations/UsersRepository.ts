c1cbb8e091f93f7f6f2a49c8456ee29b38a2953a
jvkass <jvcnmata@gmail.com>
2022-06-11 18:32:19 -0300

    bearer token na rota de especification

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -8,14 +8,8 @@ class UsersRepository implements IUsersRepository {
 
     constructor() {
         this.repository = getRepository(User);
-    }
+    } 
     
-    async findByEmail(email: string): Promise<User> {
-        const user = await this.repository.findOne({email});
-
-        return user;
-    }
-
     async create({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
         const user = this.repository.create({
             name, 
@@ -25,6 +19,18 @@ class UsersRepository implements IUsersRepository {
         });
 
         await this.repository.save(user);
+    }
+
+    async findById(id: string): Promise<User> {
+        const user = await this.repository.findOne(id);
+
+        return user;
+    }
+    
+    async findByEmail(email: string): Promise<User> {
+        const user = await this.repository.findOne({email});
+
+        return user;
     }
 
     
